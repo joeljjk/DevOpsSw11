a784f5ef1f8d64450e9b90276a27d23742bc6c6b
Kurmajoe <kurmajoe@students.zhaw.ch>
2024-05-19 12:07:36 +0200

    Zeitsteuerung

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello World by Joeljk. This was somehow deployed using Docker and Jenkins! Now even with linked Jenkins Jobs!');
+res.send('Hello World by Joeljk. This was somehow deployed using Docker and Jenkins! Now even with linked Jenkins Jobs! Schlussendlich auch Zeitgesteuert!');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
